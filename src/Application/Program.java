fa1ae33f0e1cff7fb32fb13eb2946c8e17a1dc4a
Roberto Frisanco <frisancoroberto@gmail.com>
2022-04-09 16:04:51 -0300

    First class:Position

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,9 +1,12 @@
 package Application;
 
+import BoardGame.Position;
+
 public class Program {
     public static void main (String[] args){
 
-        System.out.println("Hello World");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
 
 
     }
---
+++ src/BoardGame/Position.java
@@ -0,0 +1,33 @@
+package BoardGame;
+
+public class Position {
+
+    private  int row;
+    private int column;
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
