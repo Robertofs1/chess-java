a8a4ed8a90cf2ea6e777c7ba0fd431bb833d7fb4
Robertofs1 <frisancoroberto@gmail.com>
2022-06-03 20:32:23 -0300

    the project is already up =)

--- src/BoardGame/Position.java
+++ src/BoardGame/Position.java
@@ -1,33 +1,38 @@
-package BoardGame;
+package boardgame;
 
 public class Position {
 
-    private  int row;
-    private int column;
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 
-    public void setRow(int row) {
-        this.row = row;
-    }
+	public int getRow() {
+		return row;
+	}
 
-    public void setColumn(int column) {
-        this.column = column;
-    }
+	public void setRow(int row) {
+		this.row = row;
+	}
 
-    public int getRow() {
-        return row;
-    }
+	public int getColumn() {
+		return column;
+	}
 
-    public int getColumn() {
-        return column;
-    }
-
-    public Position(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
-
-    @Override
-    public String toString() {
-        return row + ", " + column;
-    }
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
 }
